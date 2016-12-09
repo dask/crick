695324d66b80eab79110c185fe86c6c52a78c8be
Jim Crist <crist042@umn.edu>
2016-12-08 18:05:18 -0600

    Add docstrings

--- crick/tdigest.pyx
+++ crick/tdigest.pyx
@@ -40,7 +40,17 @@ CENTROID_DTYPE = np.dtype([('mean', np.float64), ('weight', np.uint32)],
 
 
 cdef class TDigest:
-    
+    """TDigest(compression=100.0)
+
+    An approximate histogram.
+
+    Parameters
+    ----------
+    compression : float, optional
+        The compression factor to use. Larger numbers provide more accurate
+        estimates, but use more memory. For most uses, the default should
+        suffice.
+    """
     cdef tdigest_t *tdigest
 
     def __cinit__(self, compression=100.0):
@@ -48,47 +58,92 @@ cdef class TDigest:
 
     def __dealloc__(self):
         tdigest_free(self.tdigest)
-    
+
     def __repr__(self):
         return ("TDigest<compression={0}, "
                 "count={1}>").format(self.compression, self.count())
 
     def add(self, double x, int w=1, skipna=True):
+        """add(self, x, w, skipna=True)
+
+        Add a sample to this digest.
+
+        Parameters
+        ----------
+        x : float
+            The value to add.
+        w : int, optional
+            The weight of the value to add. Default is 1.
+        skipna : bool, optional
+            If False, an error will be raised if ``x`` is ever ``NaN``.
+            Otherwise, ``NaN`` values are ignored. Default is True.
+        """
         if isnan(x) and not skipna:
             raise ValueError("NaN value encountered")
         tdigest_add(self.tdigest, x, w)
 
-    def flush(self):
-        tdigest_flush(self.tdigest)
-
     @property
     def compression(self):
+        """The compression factor for this digest"""
         return self.tdigest.compression
 
     def min(self):
-        self.flush()
+        """min(self)
+
+        The minimum value in the digest."""
+        tdigest_flush(self.tdigest)
         if self.tdigest.total_weight > 0:
             return self.tdigest.min
         return NAN
 
     def max(self):
-        self.flush()
+        """max(self)
+
+        The maximum value in the digest."""
+        tdigest_flush(self.tdigest)
         if self.tdigest.total_weight > 0:
             return self.tdigest.max
         return NAN
 
     def count(self):
+        """count(self)
+
+        The number of points in the digest."""
         return self.tdigest.total_weight + self.tdigest.buffer_total_weight
 
     def cdf(self, double x):
+        """cdf(self, x)
+
+        Compute an estimate of the fraction of all points added to this digest
+        which are <= x.
+
+        Parameters
+        ----------
+        x : float
+        """
         return tdigest_cdf(self.tdigest, x)
 
     def quantile(self, double q):
+        """quantile(self, q)
+
+        Compute an estimate of the qth percentile of the data added to
+        this digest.
+
+        Parameters
+        ----------
+        q : float
+            A number between 0 and 1 inclusive.
+        """
         return tdigest_quantile(self.tdigest, q)
 
     def centroids(self):
+        """centroids(self)
+
+        Returns a numpy array of all the centroids in the digest. Note that
+        this array is a *copy* of the internal data.
+        """
         cdef size_t n
-        self.flush()
+        tdigest_flush(self.tdigest)
         if (self.tdigest.total_weight == 0):
             n = 0
         else:
@@ -119,6 +174,20 @@ cdef class TDigest:
             self.tdigest.last = n - 1
 
     def update(self, x, w=1, skipna=True):
+        """update(self, x, w, skipna=True)
+
+        Add many samples to this digest.
+
+        Parameters
+        ----------
+        x : array_like
+            The values to add.
+        w : array_like, optional
+            The weight (or weights) of the values to add. Default is 1.
+        skipna : bool, optional
+            If False, an error will be raised if ``x`` is ever ``NaN``.
+            Otherwise, ``NaN`` values are ignored. Default is True.
+        """
         if np.isscalar(x):
             x = np.array([x])
         else:
@@ -135,6 +204,15 @@ cdef class TDigest:
                                <np.PyArrayObject*>w)
 
     def merge(self, *args):
+        """merge(self, *args)
+
+        Update this digest inplace with data from other digests.
+
+        Parameters
+        ----------
+        args : TDigests
+            TDigests to merge into this one.
+        """
         if not all(isinstance(i, TDigest) for i in args):
             raise TypeError("All arguments to merge must be TDigests")
         cdef TDigest td
