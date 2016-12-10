a8474556053218dd4106527c2590ec7dd01ace60
Jim Crist <crist042@umn.edu>
2016-12-10 11:08:50 -0600

    Expand buffer size
    - Use a larger buffer than the original implementation claims is needed.
      The original implementation claims only `ceil(pi/2 * compression)`
      space is needed, but uses a factor of 2 larger for safety. However,
      both in my implementation and in the original (with a patch to use
      `ceil(pi/2 * compression)` instead), this results in out-of-bounds
      accesses. From reading the small section in the paper on the merging
      digest implementation, they find a max size `2 * compression` instead
      (if I understand correctly). This seems to squash out-of-bounds
      accesses for now.
    - Add debug flag to build with asserts on.

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
+import sys
+
 import numpy as np
 from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 
-sourcefiles = ['crick/tdigest.pyx']
 
-extensions = [Extension("crick.tdigest", sourcefiles)]
+if '--debug' in sys.argv:
+    kwargs = {'undef_macros': ["NDEBUG"],
+              'extra_compile_args': ['-O0']}
+else:
+    kwargs = {}
+
+
+extensions = [Extension("crick.tdigest",
+                        ['crick/tdigest.pyx'],
+                        **kwargs)
+              ]
 
 setup(name='crick',
       version='0.0.1',
