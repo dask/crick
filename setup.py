209b90d54e405ec03f5c84387ea677b09fbb48e0
Jim Crist <crist042@umn.edu>
2016-12-10 11:17:28 -0600

    Add -std=c99 flag to setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from setuptools.extension import Extension
 from Cython.Build import cythonize
 
 
+kwargs = {'extra_compile_args': ['-std=c99']}
+
 if '--debug' in sys.argv:
-    kwargs = {'undef_macros': ["NDEBUG"],
-              'extra_compile_args': ['-O0']}
-else:
-    kwargs = {}
+    kwargs['undef_macros'] = ["NDEBUG"]
+    kwargs['extra_compile_args'].append('-O0')
 
 
 extensions = [Extension("crick.tdigest",
