da3921068d59b467d63499cd4de578604da6cb5b
Paul Hobson <pmhobson@gmail.com>
2022-11-16 16:13:33 -0800

    update *some* repo meta data

--- README.rst
+++ README.rst
@@ -19,11 +19,11 @@ Crick is available via ``conda`` or ``pip``:
    # Install with pip
    $ pip install crick
 
-
-.. |Travis Status| image:: https://travis-ci.org/jcrist/crick.svg?branch=master
-   :target: https://travis-ci.org/jcrist/crick
-.. |Appveyor Status| image:: https://ci.appveyor.com/api/projects/status/tribw3cgqd1pn550/branch/master?svg=true
-   :target: https://ci.appveyor.com/project/jcrist/crick
+.. .. TODO: add action badges
+.. .. |Travis Status| image:: https://travis-ci.org/jcrist/crick.svg?branch=master
+..    :target: https://travis-ci.org/jcrist/crick
+.. .. |Appveyor Status| image:: https://ci.appveyor.com/api/projects/status/tribw3cgqd1pn550/branch/master?svg=true
+..    :target: https://ci.appveyor.com/project/jcrist/crick
 .. |PyPI Badge| image:: https://img.shields.io/pypi/v/crick.svg
    :target: https://pypi.python.org/pypi/crick
 .. |Conda Badge| image:: https://anaconda.org/conda-forge/crick/badges/version.svg
--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
-import sys
 import os
+import sys
 
 import numpy.distutils.misc_util as np_misc
+from Cython import Tempita as tempita
+from Cython.Build import cythonize
 from setuptools import setup
 from setuptools.extension import Extension
-from Cython.Build import cythonize
-from Cython import Tempita as tempita
 
 import versioneer
 
-compile_args = np_misc.get_info('npymath')
-compile_args['include_dirs'].append('crick/klib')
+compile_args = np_misc.get_info("npymath")
+compile_args["include_dirs"].append("crick/klib")
 
-if '--debug' in sys.argv:
-    sys.argv.remove('--debug')
-    compile_args['undef_macros'] = ["NDEBUG"]
-    compile_args['extra_compile_args'] = ['-O0']
+if "--debug" in sys.argv:
+    sys.argv.remove("--debug")
+    compile_args["undef_macros"] = ["NDEBUG"]
+    compile_args["extra_compile_args"] = ["-O0"]
 
 
 def generate_code(templates):
     """Generate code from template files"""
     for template in templates:
         # template extention must be .in
-        assert template.endswith('.in')
+        assert template.endswith(".in")
         outfile = template[:-3]
 
-        if (os.path.exists(outfile) and
-                (not os.path.exists(template) or
-                 os.stat(template).st_mtime < os.stat(outfile).st_mtime)):
+        if os.path.exists(outfile) and (
+            not os.path.exists(template)
+            or os.stat(template).st_mtime < os.stat(outfile).st_mtime
+        ):
             # If output is present but template isn't, or if template is not
             # updated no need to generate
             continue
@@ -40,32 +41,30 @@ def generate_code(templates):
             f.write(output)
 
 
-templates = ['crick/space_saving_stubs.c.in']
+templates = ["crick/space_saving_stubs.c.in"]
 
 generate_code(templates)
 
-extensions = [Extension("crick.tdigest",
-                        ['crick/tdigest.pyx'],
-                        **compile_args),
-              Extension("crick.space_saving",
-                        ['crick/space_saving.pyx'],
-                        **compile_args),
-              Extension("crick.stats",
-                        ['crick/stats.pyx'],
-                        **compile_args)
-              ]
+extensions = [
+    Extension("crick.tdigest", ["crick/tdigest.pyx"], **compile_args),
+    Extension("crick.space_saving", ["crick/space_saving.pyx"], **compile_args),
+    Extension("crick.stats", ["crick/stats.pyx"], **compile_args),
+]
 
-setup(name='crick',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      description='High performance approximate and streaming algorithms',
-      long_description=(open('README.rst').read()
-                        if os.path.exists('README.rst') else ''),
-      keywords='streaming approximate algorithms',
-      url='https://github.com/jcrist/crick',
-      maintainer='Jim Crist',
-      maintainer_email='crist042@umn.edu',
-      license='BSD',
-      packages=['crick', 'crick.tests'],
-      ext_modules=cythonize(extensions),
-      zip_safe=False)
+setup(
+    name="crick",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="High performance approximate and streaming algorithms",
+    long_description=(
+        open("README.rst").read() if os.path.exists("README.rst") else ""
+    ),
+    keywords="streaming approximate algorithms",
+    url="https://github.com/dask/crick",
+    author="Jim Crist",
+    author_email="crist042@umn.edu",
+    license="BSD",
+    packages=["crick", "crick.tests"],
+    ext_modules=cythonize(extensions),
+    zip_safe=False,
+)
